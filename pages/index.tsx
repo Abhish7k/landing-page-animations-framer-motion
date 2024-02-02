1fc390751062d2b27ac29dd0d4dd960dcbb730dc
Abhishek <abhishekbhosale.dev@gmail.com>
2024-02-02 20:43:26 +0530

    final

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,13 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import { motion } from "framer-motion";
-import { riseWithFade } from "../utils/animations";
+import {
+  riseWithFade,
+  staggerChildren,
+  videoAnimation,
+  wordAnimation,
+} from "../utils/animations";
+import React from "react";
 
 const Home: NextPage = () => {
   return (
@@ -17,23 +23,9 @@ const Home: NextPage = () => {
       </Head>
       <Navbar />
       <main className="grid grid-cols-[3fr_1fr] py-12">
-        <motion.h1
-          className="text-8xl font-bold w-[40rem] leading-[90%] tracking-[-2px]"
-          initial={{
-            y: 50,
-            opacity: 0,
-          }}
-          animate={{
-            y: 0,
-            opacity: 1,
-          }}
-          transition={{
-            ease: "easeInOut",
-            duration: 0.7,
-          }}
-        >
-          Helper robots for a better everyday
-        </motion.h1>
+        <h1 className="text-8xl font-bold w-[40rem] leading-[90%] tracking-[-2px]">
+          <AnimatedWords title="Helper robots for a better everyday" />
+        </h1>
         <motion.div
           variants={riseWithFade}
           className="text-base leading-[150%] self-end"
@@ -42,14 +34,35 @@ const Home: NextPage = () => {
           One that can learn by itself, to help anyone with (almost) anything.
         </motion.div>
       </main>
-      <video loop autoPlay muted playsInline>
+      <motion.video loop autoPlay muted playsInline variants={videoAnimation}>
         <source src="/assets/hero-video.mp4" type="video/mp4" />
-      </video>
+      </motion.video>
 
       <footer className="flex justify-center text-sm text-zinc-400 py-12">
         <p>Copyright 2022 Robot Co. All rights reserved.</p>
       </footer>
     </motion.div>
+  );
+};
+
+type AnimatedWordsProps = {
+  title: String;
+};
+
+const AnimatedWords: React.FC<AnimatedWordsProps> = ({ title }) => {
+  return (
+    <motion.span variants={staggerChildren}>
+      {title.split(" ").map((word, idx) => (
+        <div key={idx} className="inline-flex overflow-hidden">
+          <motion.span
+            className="inline-flex overflow-hidden"
+            variants={wordAnimation}
+          >
+            {word + "\u00A0"}
+          </motion.span>
+        </div>
+      ))}
+    </motion.span>
   );
 };
 
--- utils/animations.tsx
+++ utils/animations.tsx
@@ -12,3 +12,42 @@ export const riseWithFade = {
     },
   },
 };
+
+export const videoAnimation = {
+  initial: {
+    y: 100,
+    opacity: 0,
+    scale: 0.8,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      ease: [0.6, 0.01, 0.05, 0.95],
+      duration: 1,
+    },
+  },
+};
+
+export const staggerChildren = {
+  animate: {
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+export const wordAnimation = {
+  initial: {
+    y: 100,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, 0.05, 0.95],
+      duration: 1,
+    },
+  },
+};
